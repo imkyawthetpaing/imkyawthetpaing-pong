6156693e9f8fe123448636bd8d6d7080fc625c98
Kyaw Thet Paing <100781623+imkyawthetpaing@users.noreply.github.com>
2022-05-14 20:27:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var Neumorphism = '#dde1e7';
+
+// Initialize Variables
+var GAME_SECTION = document.getElementById('game_section');
+
+GAME_SECTION.style.height = 100 + 'vh';
+GAME_SECTION.style.minHeight = 500 + 'px';
+GAME_SECTION.style.backgroundColor = Neumorphism;
